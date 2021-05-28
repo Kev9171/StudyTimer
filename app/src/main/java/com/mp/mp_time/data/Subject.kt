f5f7f0f7a2956848749088b6170dc7b4386beab0
0JIEUN0 <jieun4436@naver.com>
2021-05-28 16:49:36 +0900

    AddSubjectFragment: Add subject with more information

--- app/src/main/java/com/mp/mp_time/data/Subject.kt
+++ app/src/main/java/com/mp/mp_time/data/Subject.kt
@@ -5,7 +5,7 @@ data class Subject(
     var subName: String = "", // 과목명
     var isPage: Boolean = true, // goalInt 가 의미하는 것이 페이지 수 인지, 시간 단위인지 구별
     var goalInt: Int = 0, // 목표 페이지 수 or 시간
-    val studyTime: Float = 1.0f, // 공부할 시간. 1.0 = 1시간
+    val studyTime: Float = 1.0f, // 공부할 시간. 1.0 = 1시간 1.35 = 1시간 35분
     val breakTime: Float = 1.0f, // 쉴 시간
     var date: String = "2020-01-01",   // 날짜
     var achievedTime: String = "00:00:00",  // 공부한 시간
--- app/src/main/java/com/mp/mp_time/view/AddSubjectFragment.kt
+++ app/src/main/java/com/mp/mp_time/view/AddSubjectFragment.kt
@@ -1,19 +1,25 @@
 package com.mp.mp_time.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import com.mp.mp_time.data.Subject
 import com.mp.mp_time.databinding.FragmentAddSubjectBinding
 import com.mp.mp_time.viewmodel.StudyViewModel
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class AddSubjectFragment : Fragment() {
     var binding: FragmentAddSubjectBinding? = null
     val viewModel: StudyViewModel by activityViewModels()
+    var isPage: Boolean = true
 
     override fun onDestroyView() {
         super.onDestroyView()
@@ -35,14 +41,59 @@ class AddSubjectFragment : Fragment() {
 
     private fun init() {
         binding!!.apply {
+            // 과목명 빼고는 선택 하지 않으면 디폴트 값으로 설정
+
+            // 페이지 or 시간 선택 Spinner
+            val isPageAdapter = ArrayAdapter<String>(
+                    requireContext(),
+                    android.R.layout.simple_spinner_dropdown_item,
+                    mutableListOf("페이지", "시간"))
+            isPageSpinner.adapter = isPageAdapter
+            isPageSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    isPage = position == 0 // 페이지 선택
+                }
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                }
+            }
+
+            // 시간과 분 선택 Spinner
+            val hourSpinnerAdapter = ArrayAdapter<Int>(
+                    requireContext(),
+                    android.R.layout.simple_spinner_dropdown_item,
+                    mutableListOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15))
+            val minSpinnerAdapter = ArrayAdapter<Int>(
+                    requireContext(),
+                    android.R.layout.simple_spinner_dropdown_item,
+                    mutableListOf(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55))
+            studyHourSpinner.adapter = hourSpinnerAdapter
+            studyMinSpinner.adapter = minSpinnerAdapter
+            breakHourSpinner.adapter = hourSpinnerAdapter
+            breakMinSpinner.adapter = minSpinnerAdapter
+
             addBtn.setOnClickListener {
                 if(inputSubjectName.text.toString() == ""){
                     Toast.makeText(requireContext(), "과목명을 입력해주세요.", Toast.LENGTH_SHORT).show()
                 }
                 else {
                     // 과목 등록
+                    val studyTime: Float =
+                            studyHourSpinner.selectedItem.toString().toInt() +
+                                    studyMinSpinner.selectedItem.toString().toFloat() / 100
+                    val breakTime: Float =
+                            breakHourSpinner.selectedItem.toString().toInt() +
+                                    breakMinSpinner.selectedItem.toString().toFloat() / 100
+
+                    val date = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE) // 오늘 날짜. 'yyyy-mm-dd' 형식
+
                     viewModel.insertSubject(Subject(
-                        subName = inputSubjectName.text.toString()
+                            subName = inputSubjectName.text.toString(),
+                            isPage = isPage,
+                            goalInt = inputGoalInt.text.toString().toInt(),
+                            studyTime= studyTime,
+                            breakTime= breakTime,
+                            date = date,
+                            achievedTime = "00:00:00"
                     ))
 
                     // fragment 종료
