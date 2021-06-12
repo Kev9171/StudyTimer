2ba1ca58fcad722518c350f536baf467f8db834d
0JIEUN0 <jieun4436@naver.com>
2021-06-12 14:05:38 +0900

    Fix Error: Show schedule/d-day list and Add DdayAdapter

--- app/src/main/java/com/mp/mp_time/adapter/ScheduleAdapter.kt
+++ app/src/main/java/com/mp/mp_time/adapter/ScheduleAdapter.kt
@@ -1,21 +1,15 @@
 package com.mp.mp_time.adapter
 
-import android.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Filterable
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.mp.mp_time.data.Schedule
-import com.mp.mp_time.databinding.RowDDayBinding
 import com.mp.mp_time.databinding.RowScheduleBinding
-import java.text.SimpleDateFormat
 import java.util.*
 
 class ScheduleAdapter(var items: MutableList<Schedule>): RecyclerView.Adapter<ScheduleAdapter.ViewHolder>() {
     var itemClickListener: OnItemClickListener? = null
-    val ONE_DAY = 24 * 60 * 60 * 1000
     lateinit var date: Date
     var curPos: Int = 0
     var hasContent: Boolean = false
@@ -24,19 +18,8 @@ class ScheduleAdapter(var items: MutableList<Schedule>): RecyclerView.Adapter<Sc
         fun onDateClick(holder: ScheduleAdapter.ViewHolder, view: View, data: Schedule)
     }
 
-    inner class ViewHolder(val binding: RowDDayBinding, val sbinding: RowScheduleBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(val sbinding: RowScheduleBinding) : RecyclerView.ViewHolder(sbinding.root) {
         init {
-            binding.root.setOnClickListener {
-                //클릭 이벤트 처리
-                val selectedSchedule = items[position]
-                curPos = position
-                if(selectedSchedule.dDay){
-                    val formatter = SimpleDateFormat("yyyy-M-dd")
-                    date = formatter.parse(selectedSchedule.date)
-                    hasContent = !selectedSchedule.content.isNullOrBlank()
-                    itemClickListener?.onDateClick(this, it, items[adapterPosition])
-                }
-            }
             sbinding.root.setOnClickListener {
                 if(sbinding.scheduleContent.visibility == View.GONE){
                     sbinding.scheduleContent.visibility = View.VISIBLE
@@ -48,38 +31,22 @@ class ScheduleAdapter(var items: MutableList<Schedule>): RecyclerView.Adapter<Sc
         }
     }
 
-    fun moveItem(oldPos: Int, newPos: Int) {
-        val item = items[oldPos]
-        items.removeAt(oldPos)
-        items.add(newPos, item)
-        notifyItemMoved(oldPos, newPos)
-    }
-
-    fun removeItem(pos: Int) {
-        items.removeAt(pos)
-        notifyItemRemoved(pos)
+    fun addItem(schedule: Schedule) {
+        items.add(schedule)
+        notifyDataSetChanged()
     }
 
     override fun getItemViewType(position: Int): Int {
         return position
     }
 
-
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val ddayview = RowDDayBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         val scheduleview = RowScheduleBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(ddayview, scheduleview)
+        return ViewHolder(scheduleview)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.apply {
-            val date = items[position].date.split("-")
-            if(items[position].dDay)
-                testName.text = items[position].title
-                testDate.text = getDday(date[0].toInt(), date[1].toInt(), date[2].toInt())
-        }
-        holder.sbinding.apply {
+       holder.sbinding.apply {
             if(!items[position].content.isNullOrBlank()){
                 scheduleTitle.text = items[position].title
                 scheduleDate.text = items[position].date
@@ -90,29 +57,5 @@ class ScheduleAdapter(var items: MutableList<Schedule>): RecyclerView.Adapter<Sc
 
     override fun getItemCount(): Int {
         return items.size
-
-    }
-
-    private fun getDday(dyear: Int, dmonth: Int, dday: Int): String? {
-        // D-day 설정
-        val ddayCalendar: Calendar = Calendar.getInstance()
-        ddayCalendar.set(dyear, dmonth-1, dday)
-
-        // D-day 를 구하기 위해 millisecond 으로 환산하여 d-day 에서 today 의 차를 구한다.
-        val dday: Long = ddayCalendar.timeInMillis / ONE_DAY
-        val today: Long = Calendar.getInstance().timeInMillis / ONE_DAY
-        var result = dday - today
-
-        // 출력 시 d-day 에 맞게 표시
-        val strFormat: String
-        if (result > 0) {
-            strFormat = "D-%d"
-        } else if (result == 0L) {
-            strFormat = "D-Day"
-        } else {
-            result *= -1
-            strFormat = "D+%d"
-        }
-        return String.format(strFormat, result)
     }
 }
--- app/src/main/java/com/mp/mp_time/view/CalendarFragment.kt
+++ app/src/main/java/com/mp/mp_time/view/CalendarFragment.kt
@@ -1,6 +1,7 @@
 package com.mp.mp_time.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +12,7 @@ import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.mp.mp_time.adapter.DdayAdapter
 import com.mp.mp_time.adapter.ScheduleAdapter
 import com.mp.mp_time.data.Schedule
 import com.mp.mp_time.database.ScheduleDBHelper
@@ -22,10 +24,10 @@ import java.util.*
 
 class CalendarFragment : Fragment() {
     var binding: FragmentCalendarBinding? = null
-    lateinit var scheduleDBHelper: ScheduleDBHelper
-    val scheduleList = mutableListOf<Schedule>()
     val viewModel: StudyViewModel by activityViewModels()
-    lateinit var adapter: ScheduleAdapter
+    lateinit var scheduleAdapter: ScheduleAdapter
+    lateinit var dDayAdapter: DdayAdapter
+    var curDate = ""
 
     override fun onDestroyView() {
         super.onDestroyView()
@@ -33,8 +35,8 @@ class CalendarFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View {
         binding = FragmentCalendarBinding.inflate(layoutInflater, container, false)
         return binding!!.root
@@ -48,29 +50,37 @@ class CalendarFragment : Fragment() {
     private fun init() {
         binding!!.apply {
             DdayRecyclerView.layoutManager = LinearLayoutManager(
-                requireContext(),
-                LinearLayoutManager.VERTICAL,
-                false
+                    requireContext(),
+                    LinearLayoutManager.VERTICAL,
+                    false
             )
             ScheduleRecyclerView.layoutManager = LinearLayoutManager(
-                requireContext(),
-                LinearLayoutManager.VERTICAL,
-                false
+                    requireContext(),
+                    LinearLayoutManager.VERTICAL,
+                    false
             )
+            swipeLayout.isSwipeEnabled = true
+            swipeLayout.open(swipeLayout.dragEdge)
 
-            adapter = ScheduleAdapter(viewModel.scheduleList)
-            val formatter = SimpleDateFormat("yyyy-M-dd")
+            // scheduleAdapter: 일정
+            scheduleAdapter = ScheduleAdapter(mutableListOf())
+            ScheduleRecyclerView.adapter = scheduleAdapter
+            val formatter = SimpleDateFormat("yyyy-MM-dd")
             val date = Date(System.currentTimeMillis())
-            var curSchedule = viewModel.getScheduleByDate(formatter.format(date).toString())
-            //Toast.makeText(binding!!.root.context, formatter.format(date).toString(), Toast.LENGTH_SHORT).show()
-            if(!curSchedule.isNullOrEmpty()){
-                ScheduleRecyclerView.visibility = View.VISIBLE
-                adapter.items.filter {schedule: Schedule -> schedule.equals(curSchedule)}
-                adapter.notifyDataSetChanged()
+            //var curSchedule = viewModel.getScheduleByDate(formatter.format(date).toString())
+            val curSchedule = viewModel.scheduleList.filter {
+                it.date == curDate
+            } as MutableList<Schedule>
+
+            Toast.makeText(binding!!.root.context, formatter.format(date).toString(), Toast.LENGTH_SHORT).show() // ddd
+
+            if (!curSchedule.isNullOrEmpty()) {
+                scheduleAdapter.items = curSchedule
+                scheduleAdapter.notifyDataSetChanged()
 //                adapter.scheduleDate.text = curSchedule[0].date
 //                scheduleTitle.text = curSchedule[0].title
 //                scheduleContent.text = curSchedule[0].content
-                swipeLayout.isSwipeEnabled=true
+                //swipeLayout.isSwipeEnabled = true
             }
 
             addScheduleBtn.setOnClickListener {
@@ -104,94 +114,105 @@ class CalendarFragment : Fragment() {
 
             calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
 
-                val curDate = year.toString().plus("-") + (month + 1).toString()
-                    .plus("-") + dayOfMonth.toString()
-                curSchedule = viewModel.getScheduleByDate(curDate)
+                curDate = year.toString().plus("-") + (month + 1).toString()
+                        .plus("-") + dayOfMonth.toString()
+
+                //val curSchedule = viewModel.getScheduleByDate(curDate)
+                val curSchedule = viewModel.scheduleList.filter {
+                    it.date == curDate
+                } as MutableList<Schedule>
+
                 if (!curSchedule.isNullOrEmpty()) {
-                    ScheduleRecyclerView.visibility = View.VISIBLE
-                    adapter.items.filter {schedule: Schedule -> schedule.equals(curSchedule)}
-                    adapter.notifyDataSetChanged()
+                    scheduleAdapter.items = curSchedule
+                    scheduleAdapter.notifyDataSetChanged()
 //                    scheduleDate.text = curSchedule!![0].date
 //                    scheduleTitle.text = curSchedule!![0].title
 //                    scheduleContent.text = curSchedule!![0].content
-                    swipeLayout.isSwipeEnabled = true
-                    swipeLayout.open(swipeLayout.dragEdge)
+                    //swipeLayout.isSwipeEnabled = true
+                    //swipeLayout.open(swipeLayout.dragEdge)
                 } else {
 //                    scheduleDate.text = ""
 //                    scheduleTitle.text = ""
 //                    scheduleContent.text = ""
-                    ScheduleRecyclerView.visibility = View.GONE
-                    swipeLayout.isSwipeEnabled = false
+                    //ScheduleRecyclerView.visibility = View.GONE
+                    //swipeLayout.isSwipeEnabled = false
+                    scheduleAdapter.items = mutableListOf()
+                    scheduleAdapter.notifyDataSetChanged()
                 }
-
-                addBtn.setOnClickListener {
-                    if (nameEdit.text.toString() == "")
-                        Toast.makeText(requireContext(), "과목명을 입력해주세요.", Toast.LENGTH_SHORT).show()
-                    else {
-                        var dDay = dDayCheck.isChecked
-                        viewModel.insertSchedule(
-                            Schedule(
-                                date = curDate,
-                                title = nameEdit.text.toString(),
-                                content = scheduleEdit.text.toString(),
-                                dDay = dDay
-                            )
-                        )
-                        nameEdit.text.clear()
-                        scheduleEdit.text.clear()
-
-                        addBtn.visibility = View.GONE
-                        cancelBtn.visibility = View.GONE
-                        dDayCheck.visibility = View.GONE
-                        addSchedule.visibility = View.GONE
-                        addScheduleBtn.visibility = View.VISIBLE
-                        swipeLayout.visibility = View.VISIBLE
-                        Toast.makeText(binding!!.root.context, "일정을 추가했습니다.", Toast.LENGTH_SHORT).show()
-                    }
-                }
-
             }
 
-            val simpleCallBack = object: ItemTouchHelper.SimpleCallback(
-                ItemTouchHelper.DOWN or ItemTouchHelper.UP,
-                ItemTouchHelper.RIGHT
-            ){
+            addBtn.setOnClickListener {
+                if (nameEdit.text.toString() == "")
+                    Toast.makeText(requireContext(), "일정을 입력해주세요.", Toast.LENGTH_SHORT).show()
+                else {
+                    var dDay = dDayCheck.isChecked
+
+                    val newSchedule = Schedule(
+                            date = curDate,
+                            title = nameEdit.text.toString(),
+                            content = scheduleEdit.text.toString(),
+                            dDay = dDay
+                    )
+                    viewModel.insertSchedule(newSchedule)
+
+                    nameEdit.text.clear()
+                    scheduleEdit.text.clear()
+
+                    addBtn.visibility = View.GONE
+                    cancelBtn.visibility = View.GONE
+                    dDayCheck.visibility = View.GONE
+                    addSchedule.visibility = View.GONE
+                    addScheduleBtn.visibility = View.VISIBLE
+                    swipeLayout.visibility = View.VISIBLE
+                    Toast.makeText(binding!!.root.context, "일정을 추가했습니다.", Toast.LENGTH_SHORT).show()
+
+                    scheduleAdapter.addItem(newSchedule)
+                    if(dDay) dDayAdapter.addItem(newSchedule)
+                }
+            }
+
+            dDayAdapter = DdayAdapter(viewModel.scheduleList.filter { it.dDay } as MutableList<Schedule>)
+            val simpleCallBack = object : ItemTouchHelper.SimpleCallback(
+                    ItemTouchHelper.DOWN or ItemTouchHelper.UP,
+                    ItemTouchHelper.RIGHT
+            ) {
                 override fun onMove(
-                    recyclerView: RecyclerView,
-                    viewHolder: RecyclerView.ViewHolder,
-                    target: RecyclerView.ViewHolder
+                        recyclerView: RecyclerView,
+                        viewHolder: RecyclerView.ViewHolder,
+                        target: RecyclerView.ViewHolder
                 ): Boolean {
-                    adapter.moveItem(viewHolder.adapterPosition, target.adapterPosition)
+                    dDayAdapter.moveItem(viewHolder.adapterPosition, target.adapterPosition)
                     return true
                 }
-                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
 
-                    val date = viewModel.scheduleList[viewHolder.adapterPosition].date
-                    val title = viewModel.scheduleList[viewHolder.adapterPosition].title
-                    viewModel.deleteScheduleByDateAndTitle(date, title)
-                    adapter.removeItem(viewHolder.adapterPosition)
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    val schedule = dDayAdapter.removeItem(viewHolder.adapterPosition)
+                    viewModel.deleteScheduleByDateAndTitle(schedule.date, schedule.title)
+
                     Toast.makeText(binding!!.root.context, "일정을 삭제하였습니다.", Toast.LENGTH_SHORT).show()
                 }
             }
             val itemTouchHelper = ItemTouchHelper(simpleCallBack)
             itemTouchHelper.attachToRecyclerView(DdayRecyclerView)
 
-            adapter.itemClickListener = object : ScheduleAdapter.OnItemClickListener {
-                override fun onDateClick(holder: ScheduleAdapter.ViewHolder, view: View, data: Schedule) {
-                    calendarView.date = adapter.date.time
+            dDayAdapter.itemClickListener = object : DdayAdapter.OnItemClickListener {
+                override fun onDateClick(holder: DdayAdapter.ViewHolder, view: View, data: Schedule) {
+                    calendarView.date = dDayAdapter.date.time
 
-                    if(adapter.hasContent) {
+                    if (dDayAdapter.hasContent) {
 //                        scheduleDate.text = viewModel.scheduleList[adapter.curPos].date
 //                        scheduleTitle.text = viewModel.scheduleList[adapter.curPos].title
 //                        scheduleContent.text = viewModel.scheduleList[adapter.curPos].content
-                        swipeLayout.isSwipeEnabled = true
-                        adapter.items.filter {schedule: Schedule -> schedule.equals(viewModel.scheduleList[adapter.curPos])}
-                        adapter.notifyDataSetChanged()
+                        //swipeLayout.isSwipeEnabled = true
+                        scheduleAdapter.items = viewModel.scheduleList.filter {
+                            it.date == data.date
+                        } as MutableList<Schedule>
+                        scheduleAdapter.notifyDataSetChanged()
                         swipeLayout.open(swipeLayout.dragEdge)
                     }
                 }
             }
-            DdayRecyclerView.adapter = adapter
+            DdayRecyclerView.adapter = dDayAdapter
         }
     }
 
